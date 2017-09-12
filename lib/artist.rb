b8de53ef6344bde9c808e41ab5a9b8f6af2cc046
Justin Tooley <jstooley823@gmail.com>
2017-09-12 07:12:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_accessor :name
-   @@song_count = []
+   @@song_count = 0
   def initialize(name)
     @name = name
     @songs = []
@@ -8,7 +8,7 @@ class Artist
 
   def add_song(song)
     @songs << song
-    @@song_count << song
+    @@song_count +=1
     song.artist = self
   end
 
@@ -22,7 +22,7 @@ class Artist
   end
 
   def self.song_count
-    @@song_count.length
+    @@song_count
   end
 
 
